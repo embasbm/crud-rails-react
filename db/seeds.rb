8905a73623a63d7dcaf0488aae3751e56a48a016
Emba Moussa <embasbm@gmail.com>
2021-11-16 13:40:52 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Beer.create(brand: 'Double Stout', style: 'Stout', country: 'England', quantity: 54)
+Beer.create(brand: 'Spaten', style: 'Helles', country: 'Germany', quantity: 3)
+Beer.create(brand: 'Newcastle', style: 'Brown ale', country: 'UK', quantity: 12)
