0c5eb1b7169c20968be69fd0717871d8d6c16f70
Emba Moussa <embasbm@gmail.com>
2021-11-17 15:04:14 +0100

    adapting the rails side

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :beers
+  root 'beers#index'
+  # resources :beers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
